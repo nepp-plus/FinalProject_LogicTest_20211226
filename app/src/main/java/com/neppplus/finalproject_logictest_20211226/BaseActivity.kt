8fa312d069efbe72aa91f9ac2ec4f359359637ab
nepp-plus <nepp.plus@gmail.com>
2021-12-26 10:14:33 +0900

    BaseActivity 클래스 제작

---
+++ app/src/main/java/com/neppplus/finalproject_logictest_20211226/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.neppplus.finalproject_logictest_20211226
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// 상속 허용 방법 -> 1. open,  2. abstract
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    lateinit var mContext: Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mContext = this
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
