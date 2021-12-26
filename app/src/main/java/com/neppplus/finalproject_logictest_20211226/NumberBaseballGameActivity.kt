83904523facdf1df17513761731ce9719bcc2cf3
nepp-plus <nepp.plus@gmail.com>
2021-12-26 13:55:56 +0900

    리싸이클러 어댑터 붙이기

--- app/src/main/java/com/neppplus/finalproject_logictest_20211226/NumberBaseballGameActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_logictest_20211226/NumberBaseballGameActivity.kt
@@ -3,6 +3,8 @@ package com.neppplus.finalproject_logictest_20211226
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.neppplus.finalproject_logictest_20211226.adapters.ChatAdapter
 import com.neppplus.finalproject_logictest_20211226.databinding.ActivityNumberBaseballGameBinding
 import com.neppplus.finalproject_logictest_20211226.datas.ChatData
 
@@ -11,6 +13,8 @@ class NumberBaseballGameActivity : BaseActivity() {
     lateinit var binding: ActivityNumberBaseballGameBinding
 
     val mChatList = ArrayList<ChatData>()
+
+    lateinit var mAdapter : ChatAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,11 @@ class NumberBaseballGameActivity : BaseActivity() {
         mChatList.add( ChatData("CPU", "숫자 야구게임에 오신것을 환영합니다.") )
         mChatList.add( ChatData("CPU", "3자리 숫자로 문제가 생성되었습니다.") )
         mChatList.add( ChatData("CPU", "밑의 입력칸을 이용해 3자리 숫자를 맞춰주세요.") )
+
+
+        mAdapter = ChatAdapter(mContext, mChatList)
+        binding.chattingRecyclerView.adapter = mAdapter
+        binding.chattingRecyclerView.layoutManager = LinearLayoutManager(mContext)
 
     }
 }
